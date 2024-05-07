b34fe46c7de1c2f101bdd46d441217b7c096fa0d
Nicolas Presseault <npresseault@mirego.com>
2024-05-07 12:22:05 -0400

    Make it work on unix too

--- buildDocs.sh
+++ buildDocs.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+if [[ "$OSTYPE" == "darwin"* ]]; then SEDOPTION='-i \x27\x27'; else SEDOPTION='-i'; fi;
+
 DOCS_ROOT=build/docs
 
 [ -d $DOCS_ROOT ] && rm -r $DOCS_ROOT
@@ -21,7 +23,5 @@ cp CHANGELOG.md $DOCS_ROOT/changelog.md
 cp LICENSE.md $DOCS_ROOT/license.md
 cp CODE_OF_CONDUCT.md $DOCS_ROOT/code-of-conduct.md
 
-sed -i '' -e 's/LICENSE.md/license/' $DOCS_ROOT/index.md
-
-# Convert docs/xxx.md links to just xxx/
-sed -i '' -e 's/docs\/\([a-zA-Z-]*\).md/\1/' $DOCS_ROOT/index.md
+sed $SEDOPTION -e 's/LICENSE.md/license/' $DOCS_ROOT/index.md
+sed $SEDOPTION -e 's/docs\/\([a-zA-Z-]*\).md/\1/' $DOCS_ROOT/index.md

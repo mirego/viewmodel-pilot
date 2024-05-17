d6e421d7f8e966da379f397c82fb0a3c18008349
Nicolas Presseault <npresseault@mirego.com>
2024-05-17 12:16:30 -0400

    Add more test, while at it

--- navigation/common/src/commonTest/kotlin/com/mirego/pilot/navigation/DefaultPilotNavigationManagerTest.kt
+++ navigation/common/src/commonTest/kotlin/com/mirego/pilot/navigation/DefaultPilotNavigationManagerTest.kt
@@ -53,14 +53,42 @@ class DefaultPilotNavigationManagerTest {
         assertThat(childListener.routes).isEmpty()
     }
 
+    @Test
+    fun `when pop inclusive it should pop the given route`() {
+        parentNavigationManager.push(TestNavigationRoute.Route1)
+        parentNavigationManager.push(TestNavigationRoute.Route2)
+        parentNavigationManager.push(TestNavigationRoute.Route3)
+
+        parentNavigationManager.popToId(TestNavigationRoute.Route2.uniqueId, inclusive = true)
+        assertThat(parentListener.routes).containsExactly(TestNavigationRoute.Route1)
+
+        parentNavigationManager.popToName(TestNavigationRouteName.ROUTE1.name, inclusive = true)
+        assertThat(parentListener.routes).isEmpty()
+    }
+
+    @Test
+    fun `when pop exclusive it should not pop the given route`() {
+        parentNavigationManager.push(TestNavigationRoute.Route1)
+        parentNavigationManager.push(TestNavigationRoute.Route2)
+        parentNavigationManager.push(TestNavigationRoute.Route3)
+
+        parentNavigationManager.popToId(TestNavigationRoute.Route2.uniqueId, inclusive = false)
+        assertThat(parentListener.routes).containsExactly(TestNavigationRoute.Route1, TestNavigationRoute.Route2)
+
+        parentNavigationManager.popToName(TestNavigationRoute.Route1.name, inclusive = false)
+        assertThat(parentListener.routes).containsExactly(TestNavigationRoute.Route1)
+    }
+
     private enum class TestNavigationRouteName {
         ROUTE1,
         ROUTE2,
+        ROUTE3,
     }
 
     private sealed class TestNavigationRoute(routeName: TestNavigationRouteName) : EnumPilotNavigationRoute(routeName) {
         data object Route1 : TestNavigationRoute(TestNavigationRouteName.ROUTE1)
         data object Route2 : TestNavigationRoute(TestNavigationRouteName.ROUTE2)
+        data object Route3: TestNavigationRoute(TestNavigationRouteName.ROUTE3)
     }
 
     private class TestNavigationListener : PilotNavigationListener<TestNavigationRoute>() {
@@ -77,7 +105,10 @@ class DefaultPilotNavigationManagerTest {
 
         override fun popTo(route: TestNavigationRoute, inclusive: Boolean) {
             while (routes.last() != route) {
-                routes.removeLast()
+                routes.removeLastOrNull()
+            }
+            if (inclusive) {
+                routes.removeLastOrNull()
             }
         }
     }

a14e5944f350705b707c57953cc02cd584a52c1a
Dominic Plouffe <dominic.plouffe@loto-quebec.com>
2025-04-30 05:26:59 -0400

    ktlint

--- components/android/material3/src/main/kotlin/com/mirego/pilot/components/ui/material3/PilotAlertDialog.kt
+++ components/android/material3/src/main/kotlin/com/mirego/pilot/components/ui/material3/PilotAlertDialog.kt
@@ -2,6 +2,7 @@ package com.mirego.pilot.components.ui.material3
 
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,12 +11,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.window.DialogProperties
+import com.mirego.pilot.components.content.PilotLocalImageContent
 import com.mirego.pilot.components.navigation.PilotAlertDialog
+import com.mirego.pilot.components.ui.pilotImageResourcePainter
 
 @Composable
 public fun PilotAlertDialog(
     alertDialog: PilotAlertDialog,
     modifier: Modifier = Modifier,
+    icon: @Composable (PilotLocalImageContent) -> Unit = { DefaultIcon(it) },
+    title: @Composable (String) -> Unit = { Text(text = it) },
+    text: @Composable (String) -> Unit = { Text(text = it) },
+    confirmButton: @Composable (PilotAlertDialog.Button, close: () -> Unit) -> Unit = { button, close -> DefaultButton(button, close) },
+    dismissButton: @Composable (PilotAlertDialog.Button, close: () -> Unit) -> Unit = { button, close -> DefaultButton(button, close) },
     shape: Shape = AlertDialogDefaults.shape,
     containerColor: Color = AlertDialogDefaults.containerColor,
     iconContentColor: Color = AlertDialogDefaults.iconContentColor,
@@ -23,21 +31,15 @@ public fun PilotAlertDialog(
     textContentColor: Color = AlertDialogDefaults.textContentColor,
     tonalElevation: Dp = AlertDialogDefaults.TonalElevation,
     properties: DialogProperties = DialogProperties(),
-    onDismissRequest: () -> Unit
+    onDismissRequest: () -> Unit,
 ) {
     AlertDialog(
-        icon = {
-
-        },
-        title = {
-            Text(text = alertDialog.title)
-        },
-        text = alertDialog.text?.let { { Text(text = it) } },
+        icon = alertDialog.icon?.let { { icon(it) } },
+        title = { title(alertDialog.title) },
+        text = alertDialog.text?.let { { text(it) } },
         onDismissRequest = onDismissRequest,
-        confirmButton = {
-            PilotAlertDialogButton(alertDialog.confirmButton)
-        },
-        dismissButton = alertDialog.dismissButton?.let { { PilotAlertDialogButton(it) } },
+        confirmButton = { confirmButton(alertDialog.confirmButton, onDismissRequest) },
+        dismissButton = alertDialog.dismissButton?.let { { dismissButton(it, onDismissRequest) } },
         modifier = modifier,
         shape = shape,
         containerColor = containerColor,
@@ -45,18 +47,25 @@ public fun PilotAlertDialog(
         titleContentColor = titleContentColor,
         textContentColor = textContentColor,
         tonalElevation = tonalElevation,
-        properties = properties
+        properties = properties,
     )
 }
 
 @Composable
-private fun PilotAlertDialogButton(
-    button: PilotAlertDialog.Button
-) {
+private fun DefaultIcon(content: PilotLocalImageContent) {
+    Icon(
+        painter = pilotImageResourcePainter(content.imageResource),
+        contentDescription = content.contentDescription,
+    )
+}
+
+@Composable
+private fun DefaultButton(button: PilotAlertDialog.Button, close: () -> Unit) {
     TextButton(
         onClick = {
+            close()
             button.action()
-        }
+        },
     ) {
         Text(text = button.title)
     }
--- components/common/src/commonMain/kotlin/com/mirego/pilot/components/navigation/PilotAlertDialog.kt
+++ components/common/src/commonMain/kotlin/com/mirego/pilot/components/navigation/PilotAlertDialog.kt
@@ -7,7 +7,7 @@ public data class PilotAlertDialog(
     val title: String,
     val text: String? = null,
     val confirmButton: ConfirmButton,
-    val dismissButton: DismissButton? = null
+    val dismissButton: DismissButton? = null,
 ) {
     public interface Button {
         public val title: String
@@ -18,10 +18,10 @@ public data class PilotAlertDialog(
         override val title: String,
         val isDestructive: Boolean = false,
         override val action: () -> Unit,
-    ): Button
+    ) : Button
 
     public data class DismissButton(
         override val title: String,
         override val action: () -> Unit,
-    ): Button
+    ) : Button
 }
